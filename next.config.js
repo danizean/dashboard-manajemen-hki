52202737ef4dfddbcb96749088c569e2e743e425
halvest <hasyimadani@students.amikom.ac.id>
2025-10-03 09:05:20 +0700

    add

--- next.config.js
+++ next.config.js
@@ -7,8 +7,6 @@ const nextConfig = {
     unoptimized: true,
   },
   webpack: (config) => {
-    // Menambahkan aturan untuk menangani file .mjs dengan benar
-    // Lihat: https://github.com/vercel/next.js/issues/38863
     config.module.rules.push({
       test: /.*\.mjs$/,
       type: "javascript/auto",
